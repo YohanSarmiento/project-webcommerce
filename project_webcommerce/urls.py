4843868477c51e0fb4ee132b3f568051378183a6
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-06-03 11:08:43 -0500

    Update main

--- project_webcommerce/urls.py
+++ project_webcommerce/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls')),
+    path('', include('store.urls')),
     
 ]
 
