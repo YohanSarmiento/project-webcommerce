72a5adcc7e04096a71ebb7076d1d33727c8af516
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-06-08 20:56:25 -0500

    create a build file to deploy

--- project_webcommerce/settings.py
+++ project_webcommerce/settings.py
@@ -144,9 +144,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
 
