585677f0417fb1a05d405537bb5cb2f2c75a0e49
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-06-08 21:54:35 -0500

    Deploy settings

--- build.sh
+++ build.sh
@@ -5,11 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Convert static asset files
+python manage.py collectstatic --no-input
+
 # Apply any outstanding database migrations
 python manage.py migrate
 
-# Flush the database (this will delete all data and re-create the database)
-python manage.py flush --no-input
-
-# Convert static asset files
-python manage.py collectstatic --no-input
