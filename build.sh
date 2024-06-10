4ee0791b7264ad38ef97c4aebf6fc43ccea26da2
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-06-10 16:25:43 -0500

    update settings.py

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Apply any outstanding database migrations
+python manage.py migrate
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
-python manage.py migrate
+
 
