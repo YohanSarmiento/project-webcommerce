81ce1e265ded4d319bf053ae245f99a67fc50f59
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-06-15 15:08:12 -0500

    update deployment file

--- build.sh
+++ build.sh
@@ -5,13 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Flush the database (this will delete all data and re-create the database)
-python manage.py flush --no-input
-
 # Apply any outstanding database migrations
 python manage.py migrate
-
-python manage.py makemigrations
 
 # Convert static asset files
 python manage.py collectstatic --no-input
