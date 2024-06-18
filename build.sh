ade8fa8bac3a2ccb9eded17d6855c0f8e1068b7b
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-06-17 22:08:49 -0500

    upadte deployment file

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Flush the database (this will delete all data and re-create the database)
-python manage.py flush --no-input
-
 # Apply any outstanding database migrations
 python manage.py migrate
 
